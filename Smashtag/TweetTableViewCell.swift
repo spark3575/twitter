7bebbfcd17a3b202fb5ed766c48e1ea38271c597
Shin Park <shinpark@shins-mac-mini.home>
2017-05-08 09:37:28 -0500

    Fixed queue from blocking main thread

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -29,9 +29,29 @@ class TweetTableViewCell: UITableViewCell
         tweetTextLabel?.text = tweet?.text
         tweetUserLabel?.text = tweet?.user.description
         if let profileImageURL = tweet?.user.profileImageURL {
+            
             // FIXME: blocks main thread
-            if let imageData = try? Data(contentsOf: profileImageURL) {
-                tweetProfileImageView?.image = UIImage(data: imageData)
+//            if let imageData = try? Data(contentsOf: profileImageURL) {
+//                tweetProfileImageView?.image = UIImage(data: imageData)
+//            }
+            
+            // try? Data(contentsOf:) blocks the thread it is on
+            // we are currently on the main thread
+            // so we must dispatch that call off to a background queue
+            // we'll use one of the shared, global, concurrent background queues
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                let urlContents = try? Data(contentsOf: profileImageURL)
+                // now that we're back from blocking
+                // are we still even interested in this url (i.e. does it == self.imageURL)?
+                if let imageData = urlContents, profileImageURL == self?.tweet?.user.profileImageURL {
+                    // now we want to set the image in the UI
+                    // but we are not on the main thread right now
+                    // so we are not allowed to do UI
+                    // no problem: just dispatch the UI stuff back to the main queue
+                    DispatchQueue.main.async {
+                    self?.tweetProfileImageView?.image = UIImage(data: imageData)
+                    }
+                }
             }
         } else {
             tweetProfileImageView?.image = nil
